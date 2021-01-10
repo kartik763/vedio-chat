07bb3dc4f3040a87fccf90a1890b06836b9f5929
kartik763 <65248941+kartik763@users.noreply.github.com>
2021-01-11 03:21:15 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBhPUM0Y4jNZEJzzZUmBBQMBzZxOgMk1zs",
+  authDomain: "chatting-b2a52.firebaseapp.com",
+  databaseURL: "https://chatting-b2a52-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "chatting-b2a52",
+  storageBucket: "chatting-b2a52.appspot.com",
+  messagingSenderId: "903468912530",
+  appId: "1:903468912530:web:8281b94b72c73a1abdbb4b"
+}
+
+export default config
